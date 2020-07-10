ae19f814d160f8bfea6a073d217ab5b8db84bd0a
linghuchong777888 <linghuchong777888@126.com>
2020-07-10 16:00:26 +0800

    dugujiujian

---
+++ src/main/java/com/atguigu/huashan/Dugujiujian.java
@@ -0,0 +1,12 @@
+package com.atguigu.huashan;
+
+public class Dugujiujian {
+
+    public static void main(String[] args) {
+        System.out.println("dugu9999999999999jian");
+        System.out.println("dugu9999999999999jian");
+        System.out.println("dugu9999999999999jian");
+        System.out.println("dugu9999999999999jian");
+        /// 红色 未追踪代码   绿色 暂存区
+    }
+}
