049de7c538199f3eea2fcb64060e3c120861bb9a
linghuchong777888 <linghuchong777888@126.com>
2020-07-10 16:14:00 +0800

    dugujiujian3

--- src/main/java/com/atguigu/huashan/Dugujiujian.java
+++ src/main/java/com/atguigu/huashan/Dugujiujian.java
@@ -7,6 +7,10 @@ public class Dugujiujian {
         System.out.println("dugu9999999999999jian");
         System.out.println("dugu9999999999999jian");
         System.out.println("dugu9999999999999jian");
+        System.out.println("dugu9999999999999jian");
+        System.out.println("dugu9999999999999jian");
+        System.out.println("dugu9999999999999jian");
+
         /// 红色 ：未追踪代码   绿色： 未追踪代码首次进暂存区  黑色： 已提交本地库  蓝色： 已提交本地库后 又发生了修改且未提交
     }
 }
